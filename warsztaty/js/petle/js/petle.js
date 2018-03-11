5e800249b658ead29572cf9a753eb6a501e0b82f
Paweł Liszewski <pawelliszewski92@gmail.com>
2018-03-11 15:53:32 +0100

    Pętle

---
+++ warsztaty/js/petle/js/petle.js
@@ -0,0 +1,21 @@
+//for(var i = 1; i <= 10; i++) {
+//    console.log("Iteracja nr " + i )
+//}
+
+var tablica = ["Volvo", "Mazda", "Honda", "Audi"];
+
+//for(var i =0; i < tablica.length; i++) {
+//    console.log(tablica[i]);
+//}
+
+
+//tablica.forEach(function(element, index) {
+//    console.log("Index: " + index + " Element: " + element);
+//});
+
+
+var iter = 0;
+while(iter <25) {
+    console.log("While iteracha nr " + iter);
+    iter++;
+}
