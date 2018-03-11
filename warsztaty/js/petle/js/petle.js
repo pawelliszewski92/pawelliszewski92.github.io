f430ba6bd504ecce4e1fe0bfdaaa829604624a37
Paweł Liszewski <pawelliszewski92@gmail.com>
2018-03-11 15:57:51 +0100

    petle basics

--- warsztaty/js/petle/js/petle.js
+++ warsztaty/js/petle/js/petle.js
@@ -14,8 +14,22 @@ var tablica = ["Volvo", "Mazda", "Honda", "Audi"];
 //});
 
 
-var iter = 0;
-while(iter <25) {
-    console.log("While iteracha nr " + iter);
-    iter++;
-}
+//var iter = 0;
+//while(iter <25) {
+//    console.log("While iteracha nr " + iter);
+//    iter++;
+//}
+
+
+
+
+//najpierw robimy, potem sprawdza warunek
+
+//var iter = 1;
+//do {
+//    console.log("While iteracja nr " + iter);
+//    iter++;
+//} while (iter <= 25);
+
+
+//break i continue przerywanie i wznawianie petli
