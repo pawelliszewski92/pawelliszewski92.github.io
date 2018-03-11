4cebfc74b3a3b48b0388bdf0cc9d4686c596fc57
Paweł Liszewski <pawelliszewski92@gmail.com>
2018-03-11 15:01:40 +0100

    instrukcje warunkowe

---
+++ warsztaty/js/instrukcje_warunkowe/js/instrukcje_warunkow.js
@@ -0,0 +1,30 @@
+//instrukcja if
+
+var zmienna1 = "kot"
+var zmienna2 = "PIES"
+
+if(zmienna1 != zmienna2) {
+    console.log("Warunek spelniony");
+}
+
+
+//else
+
+if(zmienna1 == zmienna2) {
+    console.log("Warunek spelniony");
+} else {
+    console.log("Warunek niespelniony");
+}
+
+
+//else if
+
+var mojaLiczba = 111;
+
+if(mojaLiczba == 5) {
+    console.log("moja liczba to 5");
+} else if(mojaLiczba == 15) {
+    console.log("moja licba to 15");
+} else {
+    console.log("moja liczba to ani 5 ani 15");
+}
