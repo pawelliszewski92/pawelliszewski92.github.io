b26a10c9bc1f18b77295a700c58b0d1c175229d7
pawelliszewski92 <pawelliszewski92@gmail.com>
2018-03-24 16:57:34 +0100

    slider

---
+++ warsztaty/jquery/js/index.js
@@ -0,0 +1,57 @@
+$(function() {
+    
+
+
+    var slideShow = $(".slide-show");
+
+    var slideCount = $(".single-slide").length;
+
+    var slideWidth = 100 / slideCount;
+
+    var slideIndex = 0;
+
+    slideShow.css('width', slideCount*100 + "%");
+
+    slideShow.find(".single-slide").each(function(index) {
+       $(this).css({
+          "width": slideWidth + '%', "margin-left": index * slideWidth + "%" 
+       });
+    });
+    
+    $(".prev-slide").click(function() {
+       slide(slideIndex - 1); 
+    });
+     $(".next-slide").click(function() {
+       slide(slideIndex + 1); 
+    });
+    
+    function slide(newSlideIndex) {
+        if(newSlideIndex < 0 || newSlideIndex > slideCount - 1)
+            return;
+        var slideCaption = slideShow.find('.slide-caption').eq(newSlideIndex);
+        
+        slideCaption.hide();
+        
+        
+        var newMarginLeft = (newSlideIndex * (-100)) + "%";
+        
+        slideShow.animate({'margin-left': newMarginLeft}, 800, function() {
+            slideIndex = newSlideIndex;
+            slideCaption.fadeIn("fast");
+        });
+    }
+  
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
+               
