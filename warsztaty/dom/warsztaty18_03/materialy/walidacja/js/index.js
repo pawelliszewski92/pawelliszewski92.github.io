c045dd3d0f89dee07fec9cb39480144c43770bc5
pawelliszewski92 <pawelliszewski92@gmail.com>
2018-03-24 13:59:05 +0100

    walidator

---
+++ warsztaty/dom/warsztaty18_03/materialy/walidacja/js/index.js
@@ -0,0 +1,18 @@
+debugger;
+var checkboxSelect = document.getElementById("wszystkie-zgody");
+
+var checkboxAll = document.getElementsByClassName("checkbox");
+
+function setCheckboxState(lastCheckboxValue) {
+    for(i=0;i<checkboxAll.length;i++) {
+        checkboxAll[i].checked = lastCheckboxValue;
+        checkboxAll[i].disabled = lastCheckboxValue;
+    }    
+};
+
+checkboxSelect.addEventListener('change', 
+                                function(){
+    setCheckboxState(checkboxSelect.checked)});
+
+
+
