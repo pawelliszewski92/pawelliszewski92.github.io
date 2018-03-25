acd0ebfb87b4d1fe05dfad834ee527e90f458e25
Paweł Liszewski <pawelliszewski92@gmail.com>
2018-03-25 16:49:42 +0200

    walidacja 2nd pass

--- warsztaty/dom/warsztaty18_03/materialy/walidacja/js/index.js
+++ warsztaty/dom/warsztaty18_03/materialy/walidacja/js/index.js
@@ -1,4 +1,3 @@
-debugger;
 var checkboxSelect = document.getElementById("wszystkie-zgody");
 
 var checkboxAll = document.getElementsByClassName("checkbox");
@@ -14,5 +13,43 @@ checkboxSelect.addEventListener('change',
                                 function(){
     setCheckboxState(checkboxSelect.checked)});
 
+
+function clearValidation() {
+    var alerts = document.querySelectorAll(".alert");
+    for(var i = 0; i<alerts.length; i++) {
+        alerts[i].parentElement.removeChild(alerts[i]);
+    };
+};
+
+
+var submitButton = document.getElementById("submit");
+
+var inputText = document.querySelectorAll("input[type='text']");
+
+var zgoda1 = document.getElementById("zgoda-marketingowa-1");
+
+function validateForm(e) {
+    clearValidation();
+    for(var i=0;i<inputText.length;i++) {
+        if (inputText[i].value == null || inputText[i].value.trim() === '') {
+            e.preventDefault();
+            var alert = document.createElement("p");
+            alert.innerHTML = ("To pole jest wymagane");
+            alert.setAttribute("class", "alert");
+            inputText[i].parentElement.insertBefore(alert, inputText[i].nextSibling); 
+        };
+    };
+    if (zgoda1.checked == false) {
+        e.preventDefault();
+            var alert = document.createElement("p");
+            alert.innerHTML = ("To pole jest wymagane");
+            alert.setAttribute("class", "alert");
+            zgoda1.parentElement.insertBefore(alert, inputText[i].nextSibling);
+    };
+};
+
+submitButton.addEventListener('click', function(){
+   validateForm(event); 
+});
 
 
