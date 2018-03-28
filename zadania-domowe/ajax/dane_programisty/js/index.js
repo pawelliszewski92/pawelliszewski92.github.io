537b0165574f50d330551d5870be98772d6d2ac8
pawelliszewski92 <pawelliszewski92@gmail.com>
2018-03-28 23:19:28 +0200

    ajax dane programisty zadanie domowe

---
+++ zadania-domowe/ajax/dane_programisty/js/index.js
@@ -0,0 +1,17 @@
+$(function(){
+    
+    
+    $("button").on('click', function() {
+       $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(data) {
+       $("#dane-programisty").append("<p>firma: " + data.firma + "</p>"); 
+       $("#dane-programisty").append("<p>imię: " + data.imie + "</p>"); 
+       $("#dane-programisty").append("<p>nazwisko: " + data.nazwisko + "</p>"); 
+       $("#dane-programisty").append("<p>zawód: " + data.zawod + "</p>"); 
+       $("#dane-programisty").append("-------------------------"); 
+        
+    }); 
+    });
+   
+    
+    
+});
