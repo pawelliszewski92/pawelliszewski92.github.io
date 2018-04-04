dc9589f43de25e40ac11da96f66371fe7138be7d
pawelliszewski92 <pawelliszewski92@gmail.com>
2018-04-04 22:23:20 +0200

    dane programisty ajax tweaks

--- zadania-domowe/ajax/dane-programisty/js/index.js
+++ zadania-domowe/ajax/dane-programisty/js/index.js
@@ -3,11 +3,12 @@ $(function(){
     
     $("button").on('click', function() {
        $.getJSON("https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php", function(data) {
-       $("#dane-programisty").append("<p>firma: " + data.firma + "</p>"); 
-       $("#dane-programisty").append("<p>imię: " + data.imie + "</p>"); 
-       $("#dane-programisty").append("<p>nazwisko: " + data.nazwisko + "</p>"); 
-       $("#dane-programisty").append("<p>zawód: " + data.zawod + "</p>"); 
-       $("#dane-programisty").append("-------------------------"); 
+       $('body').append('<div class="dane-programisty"></div>');
+       $('.dane-programisty').append("<p>firma: " + data.firma + "</p>"); 
+       $('.dane-programisty').append("<p>imię: " + data.imie + "</p>"); 
+       $('.dane-programisty').append("<p>nazwisko: " + data.nazwisko + "</p>"); 
+       $('.dane-programisty').append("<p>zawód: " + data.zawod + "</p>"); 
+      $('.dane-programisty').append("-------------------------"); 
         
     }); 
     });
