2f8fd351594ccf40593866c153ea6067c878751b
pawelliszewski92 <pawelliszewski92@gmail.com>
2018-04-10 22:31:44 +0200

    portfolio1 bootstrap tweaks

--- zadania-domowe/bootstrap/portfolio/js/index.js
+++ zadania-domowe/bootstrap/portfolio/js/index.js
@@ -5,7 +5,7 @@ $( function(){
        var target = $(this).attr('href');
         
        $("body, html").animate(
-       {scrollTop: $(this.hash).offset().top}, 200);
+       {scrollTop: $(this.hash).offset().top}, 400);
     });
     
     
