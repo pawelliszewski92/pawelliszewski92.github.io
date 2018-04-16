3d226341cbe4672bc763907d62a672e07039f224
pawelliszewski92 <pawelliszewski92@gmail.com>
2018-04-16 22:13:54 +0200

    sass

---
+++ warsztaty/js/funkcja-rekurencyjna/js/index.js
@@ -0,0 +1,42 @@
+$(function() {
+    
+   function silnia(n) {
+//       if (n==0) {
+//           return 1
+//       }
+//       var wynik = n * silnia(n-1);
+//       return wynik;
+   }
+    console.log(silnia(0));
+    console.log(silnia(1));
+    console.log(silnia(2));
+    console.log(silnia(3));
+    console.log(silnia(4));
+    console.log(silnia(5));
+    
+    
+    
+    
+    var divBlock = $("#block");
+    var paragraph = $("p");
+    
+    console.log(divBlock.html());
+    console.log(paragraph.html());
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+});
