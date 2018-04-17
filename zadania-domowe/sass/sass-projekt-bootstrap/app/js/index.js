317a5903e81ad7475fc4cb90c3e295f4200ca0ae
pawelliszewski92 <pawelliszewski92@gmail.com>
2018-04-18 01:26:54 +0200

    sass projekt bootstrap remake

---
+++ zadania-domowe/sass/sass-projekt-bootstrap/app/js/index.js
@@ -0,0 +1,27 @@
+$( function(){
+    
+    
+    //smooth scroll
+   
+    $("a[href^='#']").on('click', function(e) {
+       e.preventDefault();
+       var target = $(this).attr('href');
+        
+       $("body, html").animate(
+       {scrollTop: $(this.hash).offset().top}, 400);
+    });
+    
+    //changing nav background
+    
+    $(window).on('scroll',function(){
+        var navOffset = $("#main-nav").offset().top;
+        
+        if (navOffset > 100) {
+            $("#main-nav").addClass("scrolled");
+        } else {
+            $("#main-nav").removeClass("scrolled");
+        }
+    })
+    
+    
+});
