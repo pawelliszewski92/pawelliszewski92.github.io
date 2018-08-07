ac1c9d708c8909752e7481b2665c6105560d8346
pawelliszewski92 <pawelliszewski92@gmail.com>
2018-08-07 17:03:34 +0200

    portfolio RWD fixes

--- portfolio/js/index.js
+++ portfolio/js/index.js
@@ -11,19 +11,6 @@ $( function(){
        {scrollTop: $(this.hash).offset().top}, 400);
     });
     
-    //changing nav background
-    
-    $(window).on('scroll',function(){
-        var navOffset = $("#main-nav").offset().top;
-        
-        if (navOffset > 100) {
-            $("#main-nav").addClass("scrolled");
-        } else {
-            $("#main-nav").removeClass("scrolled");
-        }
-    })
-    
 });
 
-console.log($("#main-nav").offset().top);
 
