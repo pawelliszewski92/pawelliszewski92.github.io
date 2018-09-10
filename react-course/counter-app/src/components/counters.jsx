aa804b64c0c2b66a34fc49d35b2e6f92328792fc
pawelliszewski92 <pawelliszewski92@gmail.com>
2018-09-10 16:23:23 +0200

    react course

---
+++ react-course/counter-app/src/components/counters.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+
+class Counters extends Component {
+  render() {
+    const {
+      onReset,
+      onDelete,
+      onDecrement,
+      onIncrement,
+      counters
+    } = this.props;
+
+    return (
+      <div>
+        <button onClick={onReset} className="btn btn-primary btn-sm">
+          Reset
+        </button>
+        {counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            onDecrement={onDecrement}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
